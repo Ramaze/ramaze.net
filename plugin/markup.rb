50f4578271a9b3167cc7d552d1fe06c5ed414a58
YorickPeterse <info@yorickpeterse.com>
2011-05-27 11:19:13 +0200

    Updated the hack for Zen::Plugin::Markup so that it doesn't mess up when displaying the tags #{ and }.

--- plugin/markup.rb
+++ plugin/markup.rb
@@ -1,20 +1,25 @@
-# Suck on that well crafted and tested code!
 module Zen
   module Plugin
     class Markup
+      include ::Ramaze::Helper::CGI
+
       def markdown(markup)
-        # Standarize newlines
+        # Standardize newlines
         markup.gsub!(/\r\n/, "\n")
 
-        # Allow the use of code blocks for specific languages
-        markup.gsub!(/```([a-zA-Z0-9_\-]+)\n/, '<pre class="\\1"><code class="\\1">')
-        markup.gsub!('```', '</code></pre>')
+        # Allow #{ and } to be used in markup without executing it
+        markup.gsub!('#{', '\#\{')
+        markup.gsub!('}' , '\}')
 
-        # Parse everything that's remaining
-        markup = RDiscount.new(markup).to_html
+        # Enable GitHub like code blocks, taken from here:
+        # https://github.com/ivanvanderbyl/markup/commit/52d09d651a423f688651a07b9df999d19e1da8f6#L0R111
+        markup.gsub!(/^``` ?([^\r\n]+)?\r?\n(.+?)\r?\n```\r?$/m) do
+          "<pre class=\"#{$1}\"><code class=\"#{$1}\">#{h($2)}</code></pre>"
+        end
 
-        return markup
+        return RDiscount.new(markup).to_html
       end
+
     end
   end
 end
